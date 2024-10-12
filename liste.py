10608ef80d67ddf53be6d08d90c30d148af34d04
zorbeyyavas <yavaszorbey@gmail.com>
2024-10-12 16:15:31 +0300

    Add files via upload

---
+++ liste.py
@@ -0,0 +1,7 @@
+#pythonda listeler demetler ve sözlükler liste farklı veri tiplerini içinde barındıran üzerinde ekleme çıkartma güncelleme vb gerçekleştiren yapılardır tanımlarken türkçe kullanma tanımlaması aşağdaki gibi
+#ilk öge 0 dır 0.index yani
+#elemanları nuömaralandrırken 0 dan başlar ancak sayarken 1 den başlar
+
+liste=[24,6,"mehmet",54]
+liste[2] #mehmettir
+print(len(liste)) #cevap 4
